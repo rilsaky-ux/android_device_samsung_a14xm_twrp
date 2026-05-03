8b89f42dc5413e631a610798bab3647aebde2c4a
rilsaky-ux <rilsaky@gmail.com>
2026-05-03 07:32:15 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,3 +108,10 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+
+TW_DEVICE_VERSION := rilsaky Stable
+SHRP_MAINTAINER := rilsaky
+OF_MAINTAINER := rilsaky
+MAINTAINER := rilsaky
+
