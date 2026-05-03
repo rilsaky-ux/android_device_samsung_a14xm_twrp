b42f9ce9cc50a2149f4103b8b39e7bb3da40cdd6
rilsaky-ux <rilsaky@gmail.com>
2026-05-03 07:20:49 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a14xm-user
-add_lunch_combo omni_a14xm-userdebug
-add_lunch_combo omni_a14xm-eng
+add_lunch_combo twrp_a14xm-user
+add_lunch_combo twrp_a14xm-userdebug
+add_lunch_combo twrp_a14xm-eng
